7439db5218cd5fb18d7e8584432a4c3aadd1a001
julekb <julek.baranski@gmail.com>
2019-04-03 18:50:18 +0200

    it just looks nicer

--- script.py
+++ script.py
@@ -3,22 +3,22 @@ from keras.layers import Dense, Flatten
 from keras.layers.embeddings import Embedding
 from keras.layers.recurrent import LSTM
 
-#Training = 500k episodes
-#1 episode = 128 games
+# Training = 500k episodes
+# 1 episode = 128 games
 
-#Test = 5 episodes 
+# Test = 5 episodes
 
-itempool = [2,7,3]
-prop_utilities = [6,2,1]
-utterance_prev = [2,4,2,1,3,5]
-item_context = [[6,2,1],[2,4,2,1,3,5]]
+itempool = [2, 7, 3]
+prop_utilities = [6, 2, 1]
+utterance_prev = [2, 4, 2, 1, 3, 5]
+item_context = [[6, 2, 1], [2, 4, 2, 1, 3, 5]]
 
 vocab_size = 11
 dim_size = 100
 max_length = 6
 
 model = Sequential()
-model.add(Embedding(vocab_size,dim_size,input_length = max_length))
+model.add(Embedding(vocab_size, dim_size, input_length=max_length))
 model.add(LSTM(100))
 model.compile(optimizer='adam', loss='mse')
 print(model.summary())
