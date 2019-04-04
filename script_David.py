e34fbb5fefcd87b88f463c14855d08edb513943b
julekb <julek.baranski@gmail.com>
2019-04-04 12:46:58 +0200

    linter

--- script_David.py
+++ script_David.py
@@ -6,40 +6,36 @@ from keras.preprocessing import sequence
 
 import numpy as np
 
-#Training = 500k episodes
-#1 episode = 128 games
+# Training = 500k episodes
+# 1 episode = 128 games
 
-#Test = 5 episodes 
+# Test = 5 episodes
 
-
-itempool = [2,7,3]
-prop_utilities = [6,2,1]
-utterance_prev = [2,4,2,1,3,5]
-item_context = np.array([6,2,1,2,4,2,1,3,5])	
-
+itempool = [2, 7, 3]
+prop_utilities = [6, 2, 1]
+utterance_prev = [2, 4, 2, 1, 3, 5]
+item_context = np.array([6, 2, 1, 2, 4, 2, 1, 3, 5])
 vocab_size = 11
 dim_size = 100
 max_length = 6
 
 model = Sequential()
-model.add(Embedding(vocab_size,dim_size))
+model.add(Embedding(vocab_size, dim_size))
 model.add(LSTM(100))
 model.compile(optimizer='adam', loss='mse')
 print(model.summary())
-utterance_prev = np.reshape(utterance_prev, (1,6))
+utterance_prev = np.reshape(utterance_prev, (1, 6))
 output_array = model.predict(item_context)
 
 
-#Policy Terminate
+# Policy Terminate
+
 model = Sequential()
-model.add(Dense(2, input_dim=100,
-	activation="sigmoid"))
+model.add(Dense(2, input_dim=100, activation="sigmoid"))
 model.compile(optimizer='adam', loss='mse')
 
 print(model.summary())
 terminate = model.predict(output_array)
 print(terminate)
 
-
-
-#model.fit(state, reward_value, epochs=1, verbose=0)
+# model.fit(state, reward_value, epochs=1, verbose=0)
