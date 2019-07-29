1e7ae6e4657f914694bf378147b7032964bbe04a
julekb <julek.baranski@gmail.com>
2019-07-29 20:43:33 +0200

    init

--- emergent/__init__.py
+++ emergent/__init__.py
@@ -1,8 +1,8 @@
 # from .settings import ProjectSettings, AgentSettings, GameSettings
-from .settings import settings
-from .game import Action, Game
 from .agents import Agent
-from .test import *
+from .data_processing import *
+from .game import Action, Game
 from .models import NumberSequenceEncoder, TerminationPolicy, UtterancePolicy, ProposalPolicy
-
+from .settings import settings
+from .test import *
 # from .agents import *
